c9c9a540bd1f5902c6145c552cf59a5ec5869c1c
Alex Tomlinson <alexdangersky@gmail.com>
2016-03-31 03:56:19 -0400

    Cooper Radio! Soon.

---
+++ audioplayer.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Create an instance
+var wavesurfer = Object.create(WaveSurfer);
+
+// Init & load audio file
+document.addEventListener('DOMContentLoaded', function () {
+    // Init
+    wavesurfer.init({
+        container: document.querySelector('#waveform'),
+        waveColor: 'black',
+        progressColor: 'gray',
+        backend: 'MediaElement',
+        hideScrollbar: 'true',
+        cursorWidth: 1,
+        height: 100,
+        normalize: true,
+        // barWidth: 2,
+    });
+
+
+    // Load audio from URL
+    wavesurfer.load('audio/minute.mp3');
+
+
+    // Play button
+    var button = document.querySelector('[data-action="play"]');
+
+    button.addEventListener('click', wavesurfer.playPause.bind(wavesurfer));
+});
---
+++ littlethings.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $(".playbutton").click(function() {
+
+    $("#transmit").toggleClass("transmitting");
+  });
+
+  $("h2").click(function() {
+
+    $("div").toggleClass("hide");
+    $("h2").toggleClass("hide");
+    $("#explorer").toggleClass("hide flyin");
+    $("#folder").toggleClass("show");
+  });
+  $(".playbutton").click(function() {
+
+    $("#playandpause").toggleClass("fa fa-play fa-lg");
+    $("#playandpause").toggleClass("fa fa-pause fa-lg");
+  });
+
+  $("h1").click(function() {
+
+    $("div").toggleClass("hide");
+    $("h2").toggleClass("hide");
+    $("#explorer").toggleClass("hide flyin");
+    $("#folder").toggleClass("show");
+  });
+
+  $(".selected").click(function() {
+
+    $("div").toggleClass("hide");
+    $("h2").toggleClass("hide");
+    $("#explorer").toggleClass("hide flyin");
+    $("#folder").toggleClass("show");
+  });
+});
